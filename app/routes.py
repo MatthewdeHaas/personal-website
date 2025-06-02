6c9935ed1048b265d2b501b7da551fa4016131fb
Matthew deHaas <mattdehaas28@gmail.com>
2025-06-02 19:53:48 -0400

    Add a structure for content.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,22 @@ from flask import Blueprint, url_for, render_template
 
 home = Blueprint('home', __name__, template_folder='templates')
 
+
 @home.route("/")
 def index():
     return render_template("index.html")
+
+
+@home.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@home.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@home.route("/blog")
+def blog():
+    return render_template("blog.html")
